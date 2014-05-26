610b6957a73e9c85b4a471603733cf846ab933c4
Sowiet <olek.motyka@gmial.com>
2014-05-26 14:51:39 +0200

    dsa

--- HealthClinic/HealthClinic/Login.Designer.cs
+++ HealthClinic/HealthClinic/Login.Designer.cs
@@ -57,7 +57,7 @@
             this.btn_Cancel.Name = "btn_Cancel";
             this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
             this.btn_Cancel.TabIndex = 1;
-            this.btn_Cancel.Text = "Anuluj";
+            this.btn_Cancel.Text = "Zamknij";
             this.btn_Cancel.UseVisualStyleBackColor = true;
             this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
             // 
